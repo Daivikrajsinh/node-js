4a66a6637c6497d78fb989bf7cb48e7902ebbc10
Daivikrajsinh <devikrajsinh05@gmail.com>
2025-06-02 13:19:18 +0530

    http-routing

---
+++ darshan/project 1/index.js
@@ -0,0 +1,36 @@
+let http = require("http")
+let PORT = 8083;
+let fs = require("fs");
+
+http.createServer((req, res) => {
+    let filename = ""
+    switch (req.url) {
+        case "/": filename = "home.html";
+        break;
+            
+
+        case "/about":filename ="about.html";
+        break;
+
+        case "/services":filename = "services.html";
+        break;
+
+        case "/contact" : filename = "contact.html";
+        break;
+        
+        default:
+            break;
+    }
+    fs.readFile(filename, (err, data) => {
+        if (!err) {
+            res.end(data)
+        }
+    })
+})
+    .listen((PORT), (error) => {
+        if (error) {
+            console.log("Server is not connected");
+            return;
+        }
+        console.log("Server is Connected",PORT)
+    })
